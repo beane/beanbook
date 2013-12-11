8789d1fd1ec3ce7b28664d1c8e46b4c5ed274a7b
App Academy Student <student@appacademy.io>
2013-12-11 09:13:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beanbook::Application.config.secret_token = '7da425dcdbde9d643e7e08a5ddbdce145d0b1860f708298a05a7bf68710662e230610920450794a2acd8c9b574efe54f4976b06bda0b1750084caf11a977253d'
