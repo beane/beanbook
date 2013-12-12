77c486bd76a17653fb4d5789fee5b411949508af
Brian Deane <bdeanemachine@gmail.com>
2013-12-12 14:23:33 -0500

    Finish photo permissions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,6 +4,22 @@ class PhotosController < ApplicationController
     render :index
   end
 
+  def new
+    render :new
+  end
+
+  def create
+    photo = Photo.new(params[:photo])
+    photo.user_id = current_user.id
+    if photo.save
+      flash[:notice] = ["Photo uploaded successfully!"]
+      redirect_to user_photo_url(params[:user_id], photo)
+    else
+      flash[:errors] = photo.errors.full_messages
+      redirect_to user_photos_url(current_user)
+    end
+  end
+
   def show
     @photo = Photo.find(params[:id])
     render :show
